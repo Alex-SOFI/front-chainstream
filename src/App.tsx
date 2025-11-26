9cf4d72ac31b8ed7aaa46fdaa6568c32a607becd
Alexandre <alexandre@mac-aborene.home>
2025-11-26 14:38:26 +0100

    Add Retail 4 page with specific header icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BlaiseLanding from './BlaiseLanding';
 import BlaiseLandingRetail from './BlaiseLandingRetail';
 import BlaiseLandingRetailLight from './BlaiseLandingRetailLight';
 import BlaiseLandingRetail3 from './BlaiseLandingRetail3';
+import BlaiseLandingRetail4 from './BlaiseLandingRetail4';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/instit" element={<BlaiseLanding />} />
         <Route path="/retail2" element={<BlaiseLandingRetailLight />} />
         <Route path="/retail3" element={<BlaiseLandingRetail3 />} />
+        <Route path="/retail4" element={<BlaiseLandingRetail4 />} />
       </Routes>
     </Router>
   );
